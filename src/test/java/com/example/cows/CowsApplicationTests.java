c48e8a15026fd20f8645e0fc881a247d3cd8e83f
diroxxx <che.dom29@gmail.com>
2025-03-06 14:57:43 +0100

    mkmk

---
+++ src/test/java/com/example/cows/CowsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CowsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
